7a45db2a7519a99ee9ba6a56d11f956dbcdbe380
rock <rock.dong@oracle.com>
2018-02-07 10:14:00 +0800

    First commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import net.glxn.qrgen.QRCode;
+import net.glxn.qrgen.image.ImageType;
+
+public class Main {
+    public static void main(String[] args) {
+        ByteArrayOutputStream out = QRCode.from("Hello World My Name is rock").to(ImageType.PNG).stream();
+
+        try {
+            FileOutputStream fout = new FileOutputStream(new File(
+                    "C:QR1_Code.JPG"));
+
+            fout.write(out.toByteArray());
+
+            fout.flush();
+            fout.close();
+
+        } catch (FileNotFoundException e) {
+            // Do Logging
+        } catch (IOException e) {
+            // Do Logging
+        	e.printStackTrace();
+        };
+    }
+}
